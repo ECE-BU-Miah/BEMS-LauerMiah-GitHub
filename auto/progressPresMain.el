61e55f1b49ce5be1112d76eb8183d2ebbb91b981
Md Suruz Miah <smiah@bradley.edu>
2020-06-25 16:25:13 -0500

    Modified latex presentation structure

---
+++ auto/progressPresMain.el
@@ -0,0 +1,22 @@
+(TeX-add-style-hook
+ "progressPresMain"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("todonotes" "colorinlistoftodos")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "latex/2020-06-25-ProgressPres"
+    "beamer"
+    "beamer10"
+    "todonotes"))
+ :latex)
+
